399e4f400c0192e245ee4c5c4856ac123ef8ba97
Alexander <sashok.tsibin@gmail.com>
2023-02-02 23:53:40 +0700

    убрал вложенность

--- app/src/main/java/ru/tsibin/countryinfo/MainActivity.kt
+++ app/src/main/java/ru/tsibin/countryinfo/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.tsibin.countryinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.countriesinfo.R
 import com.example.countriesinfo.databinding.ActivityMainBinding
 import ru.tsibin.countryinfo.data.CountryInfoRepository
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val countryInfoRepository = CountryInfoRepository()
+    private val navController get() = findNavController(R.id.fragmentHolder)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
