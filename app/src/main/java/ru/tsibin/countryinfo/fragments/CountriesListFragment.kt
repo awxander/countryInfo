7a3b54610256b468456f06f66c62e81d4310d375
Alexander <sashok.tsibin@gmail.com>
2023-02-24 16:33:05 +0700

    add recyclerView initialization to CountriesListFragment

--- app/src/main/java/ru/tsibin/countryinfo/fragments/CountriesListFragment.kt
+++ app/src/main/java/ru/tsibin/countryinfo/fragments/CountriesListFragment.kt
@@ -1,4 +1,29 @@
 package ru.tsibin.countryinfo.fragments
 
-class CountriesListFragment {
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.countriesinfo.R
+import ru.tsibin.countryinfo.adapter.CountryAdapter
+import ru.tsibin.countryinfo.data.CountryInfo
+
+class CountriesListFragment : Fragment(R.layout.countries_list_fragment) {
+
+    private val args: CountriesListFragmentArgs by navArgs()
+
+    private val countriesRecyclerView
+        get() = requireView().findViewById<RecyclerView>(R.id.countriesRecyclerView)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        countriesRecyclerView.adapter = CountryAdapter(args.countriesInfo.asList())
+    }
 }
