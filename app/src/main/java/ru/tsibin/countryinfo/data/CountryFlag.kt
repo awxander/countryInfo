04a8f48bd98cd7f4fdd66066da0e9adb78f83039
Alexander <sashok.tsibin@gmail.com>
2023-02-25 17:54:42 +0700

    make data classes parcelable

--- app/src/main/java/ru/tsibin/countryinfo/data/CountryFlag.kt
+++ app/src/main/java/ru/tsibin/countryinfo/data/CountryFlag.kt
@@ -1,9 +1,12 @@
 package ru.tsibin.countryinfo.data
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class CountryFlag(
     @SerializedName("png" ) var png : String? = null,
     @SerializedName("svg" ) var svg : String? = null,
     @SerializedName("alt" ) var alt : String? = null
-)
+) : Parcelable
